6859e658c1fc2aeca4b756fdeada71ce0c4a3442
HP <HP@USER>
2018-11-06 10:59:32 +0600

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.model;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            // MessageBox.Show("Письмо отправлено");
+            string message = "";
+            bool resultSend =
+                SendMail.SendMaiL(tbxTo.Text,
+                                  tbxCopy.Text,
+                                  tbxSubj.Text,
+                                  tbxMessageBody.Text,
+                                  out message);
+            lblMessage.Content = message;
+            if (resultSend)
+            {
+                lblMessage.Foreground =
+                    new SolidColorBrush(Colors.Green);
+            }
+            else
+                lblMessage.Foreground = new SolidColorBrush(Colors.Red);
+        }
+    }
+}
---
+++ WpfApp1/model/SendMail.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.model
+{
+    class SendMail
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="to">Кому</param>
+        /// <param name="toCopy">Копия</param>
+        /// <param name="subject">Тема</param>
+        /// <param name="body">Сбщ</param>
+        /// <param name="message">Сообщение</param>
+        /// <returns></returns>
+        public static bool SendMaiL(string to, string toCopy,
+            string subject, string body, out string message)
+        {
+            bool result = false;
+
+            MailMessage msg = new MailMessage();
+            msg.To.Add(new MailAddress(to));
+            msg.CC.Add(new MailAddress(toCopy));
+            msg.Subject = subject;
+            msg.Body = body;
+            msg.From = new MailAddress("s_mart11@mail.ru", "Step test");
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.mail.ru";//"smtp.gmail.com";
+            smtp.Port = 465;//587;
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("s_mart11@mail.ru","***********");
+
+            try
+            {
+                smtp.Send(msg);
+                message = "Сообщение отправлено";
+                result = true;
+            }
+            catch(Exception ex)
+            {
+                message = ex.Message;
+                result = false;
+            }
+            return result;
+        }
+    }
+}
